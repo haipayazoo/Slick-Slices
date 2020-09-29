c5b069af8f4712bfa10a19ee057dec9550fd94dc
Cameron Mathos <haipayazoo@gmail.com>
2020-09-29 16:12:39 -0700

    Created a Layout and Footer components

---
+++ gatsby/src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+import Footer from './Footer';
+
+export default function Layout() {
+  return (
+    <Layout>
+      <Nav />
+      <p>I am the page content</p>
+      <Footer />
+    </Layout>
+  );
+}
