b0310c1eb00e27a0d2a2c4e2fa02105c59bafc55
Cameron Mathos <haipayazoo@gmail.com>
2020-10-26 16:50:14 -0700

    More gatsby-config configurations

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,26 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+console.log(process.env.SANITY_TOKEN);
+
 export default {
   siteMetadata: {
     title: `Slick Slices`,
     siteUrl: `https://gatsby.pizza`,
     description: `The best pizza place in California!`,
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // This is the name of the plugins you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'dii3hp7a',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
