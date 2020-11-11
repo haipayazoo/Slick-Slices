246c66c6a2e9f59b7e7711352ba951e2dcee39d3
Cameron Mathos <haipayazoo@gmail.com>
2020-11-10 16:58:07 -0800

    Added topping styles for pizza filter

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -1,5 +1,29 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import styled from 'styled-components';
+
+const ToppingsStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    .active {
+      background: var(--yelow);
+    }
+  }
+`;
 
 function countPizzasInToppings(pizzas) {
   // Return the pizzas with the count
@@ -14,13 +38,15 @@ function countPizzasInToppings(pizzas) {
       if (existingTopping) {
         existingTopping.count += 1;
       }
-
       // Otherwise, create a new entry in our acc and set it to one
-      acc[topping.id] = {
-        id: topping.id,
-        name: topping.name,
-        count: 1,
-      };
+      else {
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
+      }
+
       return acc;
     }, {});
   // Sort them based on their count
@@ -57,8 +83,13 @@ export default function ToppingsFilter() {
   // Loop over the lsit of toppings and display the topping and the count of the pizzas in the topping
   // Link it up
   return (
-    <div>
-      <p>TOPPINGS</p>
-    </div>
+    <ToppingsStyles>
+      {toppingsWithCounts.map((topping) => (
+        <Link to={`/topping/${topping.name}`} key={topping.id}>
+          <span className="name">{topping.name}</span>
+          <span className="count">{topping.count}</span>
+        </Link>
+      ))}
+    </ToppingsStyles>
   );
 }
