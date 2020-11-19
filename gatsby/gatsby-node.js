7092385ef0def306da8f3bd34c7bfd6962f7d1fc
Cameron Mathos <haipayazoo@gmail.com>
2020-11-19 11:05:37 -0800

    Create gatsby-node.js

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,12 @@
+async function turnPizzasIntoPages(params) {
+  // 1. Get a template for this page
+  // 2. Query all the pizzas
+  // 3. Loop over each pizza and create a page for that pizza
+}
+
+export async function createPages(params) {
+  // Create pages dynamically
+  // 1. Pizzas
+  // 2. Toppings
+  // 3. Slicemasters
+}
