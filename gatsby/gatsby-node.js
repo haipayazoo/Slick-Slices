03471950133b784e3c1c371856f9a074cb4c8237
Cameron Mathos <haipayazoo@gmail.com>
2020-12-03 17:41:49 -0800

    Update gatsby-node.js

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -104,7 +104,7 @@ async function turnSlicemastersIntoPages({ graphql, actions }) {
     }
   `);
   // TODO: 2. Turn each slicemaster into their own page (TODO)
-  data.slicemasters.forEach((slicemaster) => {
+  data.slicemasters.nodes.forEach((slicemaster) => {
     actions.createPage({
       component: resolve('./src/templates/Slicemaster.js'),
       path: `/slicemaster/${slicemaster.slug.current}`,
@@ -114,6 +114,7 @@ async function turnSlicemastersIntoPages({ graphql, actions }) {
       },
     });
   });
+
   // 3. Figure out how many pages there are based on how many slicemasters there are and how many per page!
   const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
   const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
