d6d884249c34861bb2307a17e6e5be460fb56f92
Cameron Mathos <haipayazoo@gmail.com>
2020-12-09 09:52:43 -0800

    Gatsby SEO and head tags

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -7,8 +7,10 @@ export default {
     title: `Slick Slices`,
     siteUrl: `https://gatsby.pizza`,
     description: `The best pizza place in California!`,
+    twitter: '@slicksSlices',
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     {
       // This is the name of the plugins you are adding
--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SEO from '../components/SEO';
 
 export default function OrderPage() {
   return (
-    <div>
+    <>
+      <SEO title="Order a Pizza!" />
       <p>Hey! I'm the order page</p>
-    </div>
+    </>
   );
 }
